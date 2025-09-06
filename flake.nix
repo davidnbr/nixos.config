9c546341b44462dd9876b4ae7e261ea53bc5f07a
Davidnbr <davidnbr_98@hotmail.com>
2025-09-06 14:48:09 -0500

    New Ubuntu 24.04 config

--- flake.nix
+++ flake.nix
@@ -1,168 +1,40 @@
 {
-  description = "NixOS Configuration with Home Manager";
+  description = "Home Manager for Ubuntu - dbecerra";
 
   inputs = {
-    # NixOS stable release
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-
-    # NixOS unstable (optional, for newer packages)
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # Home Manager
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # Hardware optimizations (optional but recommended)
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
-    flake-utils.url = "github:numtide/flake-utils";
-
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    oh-my-tmux = {
-      url = "github:gpakosz/.tmux";
-      flake = false;
-    };
-
-    lazy-nvim = {
-      url = "github:LazyVim/starter";
-      flake = false;
-    };
-
-    claude-desktop = {
-      #url = "github:k3d3/claude-desktop-linux-flake";
-      url = "github:davidnbr/claude-desktop-linux-flake/549d0783f882b6d7aa48d93111c98644c03185f8";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
+    
+    devenv.url = "github:cachix/devenv";
+    oh-my-tmux = { url = "github:gpakosz/.tmux"; flake = false; };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-unstable,
-      home-manager,
-      nixos-hardware,
-      ...
-    }@inputs:
+  outputs = { nixpkgs, nixpkgs-stable, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
-      hostname = "nixos-dbecerra";
-      username = "dbecerra";
-
-      # Create pkgs with overlays for unstable packages
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
         overlays = [
-          # Add unstable packages overlay
           (final: prev: {
-            unstable = import nixpkgs-unstable {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          })
-          # Add wrapper for asdf2nix
-          (final: prev: {
+            unstable = import nixpkgs { inherit system; config.allowUnfree = true; };
+            stable = import nixpkgs-stable { inherit system; config.allowUnfree = true; };
             asdf2nix-wrapper = prev.writeShellScriptBin "asdf2nix" ''
-              #!/usr/bin/env bash
-              exec ${inputs.nixpkgs.legacyPackages.${system}.nix}/bin/nix run github:brokenpip3/asdf2nix -- "$@"
+              exec ${final.nix}/bin/nix run github:brokenpip3/asdf2nix -- "$@"
             '';
           })
         ];
       };
-    in
-    {
-      # NixOS system configurations
-      nixosConfigurations = {
-        ${hostname} = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit inputs;
-            inherit nixpkgs-unstable;
-          };
-          modules = [
-            # Your system configuration
-            ./hosts/${hostname}/configuration.nix
-            ./hosts/${hostname}/hardware-configuration.nix
-
-            # Hardware optimizations (optional)
-            # Uncomment and adjust for your hardware:
-            # nixos-hardware.nixosModules.common-cpu-intel
-            # nixos-hardware.nixosModules.common-pc-ssd
-
-            # Global configuration
-            {
-              networking.hostName = hostname;
-              nix.settings.experimental-features = [
-                "nix-command"
-                "flakes"
-              ];
-              nixpkgs.config.allowUnfree = true;
-              # Make the overlay available system-wide
-              nixpkgs.overlays = [
-                (final: prev: {
-                  unstable = import nixpkgs-unstable {
-                    inherit system;
-                    config.allowUnfree = true;
-                  };
-                })
-              ];
-
-              environment.systemPackages = with pkgs; [
-                git
-                vim
-                wget
-                curl
-              ];
-            }
-          ];
-        };
+    in {
+      homeConfigurations.dbecerra = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit inputs; };
+        modules = [ ./home/dbecerra.nix ];
       };
-
-      # Home Manager configurations
-      homeConfigurations = {
-        ${username} = home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs;
-          extraSpecialArgs = {
-            inherit inputs;
-            inherit nixpkgs-unstable;
-          };
-          modules = [
-            ./home/${username}.nix
-            {
-              home.username = username;
-              home.homeDirectory = "/home/${username}";
-              home.stateVersion = "25.05"; # Match your NixOS version
-              programs.home-manager.enable = true;
-            }
-          ];
-        };
-      };
-
-      # Development shell (optional, useful for development)
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          git
-          nixpkgs-fmt
-          nil # Nix language server
-        ];
-        shellHook = ''
-          echo "NixOS Development Environment"
-          echo "Available commands:"
-          echo "  sudo nixos-rebuild switch --flake .#${hostname}"
-          echo "  home-manager switch --flake .#${username}"
-          echo "  nix flake update"
-        '';
-      };
-      ## Commands for updates outside of the directory
-      #sudo nixhome-manager switch --flake ~/nixos-config#$(whoami)
-      #home-manager switch --flake ~/nixos-config#$(whoami)
     };
 }
--- home/dbecerra.nix
+++ home/dbecerra.nix
@@ -1,56 +1,31 @@
+{ config, pkgs, inputs, lib, ... }:
+
 {
-  config,
-  pkgs,
-  inputs,
-  lib,
-  ...
-}:
-let
-  sqlls = pkgs.nodePackages.sql-language-server or null;
-in
-{
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "dbecerra";
   home.homeDirectory = "/home/dbecerra";
-
   home.stateVersion = "25.05";
-
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    #(pkgs.nerdfonts.override { fonts = [ "Hack" ]; })
+    # Fonts
     nerd-fonts.hack
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    
+    # GUI Applications
     unstable._1password
     unstable._1password-gui
     unstable.vscode
     unstable.google-chrome
     unstable.firefox
-    unstable.slack
-    #wpsoffice
-    inputs.claude-desktop.packages.${pkgs.system}.claude-desktop-with-fhs
     
-    (unstable.python311.withPackages(ps: with ps; [
-      pip
-      httpx
-    ]))
+    # Languages and Runtimes
+    (unstable.python311.withPackages(ps: with ps; [ pip httpx ]))
     unstable.uv
     unstable.go
     unstable.nodejs_20
     unstable.ruby_3_4
     unstable.elixir_1_18
-
+    
+    # DevOps Tools
     unstable.awscli2
     unstable.gh
     unstable.terraform
@@ -58,400 +33,73 @@ in
     ansible
     ansible-lint
     unstable.aws-vault
+    
+    # Development Tools
     lazydocker
     ripgrep
     htop
     fzf
     bat
     jq
-    unstable.blesh
     tree
     xclip
+    git
+    gcc
+    unzip
+    wget
+    curl
+    fd
+
     unstable.tmux
+    unstable.starship
+    unstable.blesh
+    unstable.neovim
     unstable.pre-commit
     unstable.tldr
     nix-prefetch-github
     inputs.devenv.packages.${pkgs.system}.devenv
     unstable.claude-code
     asdf2nix-wrapper
+    
+    # Language servers (LazyVim will find them)
+    nil                # Nix LSP
+    lua-language-server # Lua LSP
+    nodePackages.typescript-language-server # JS/TS LSP
+    gopls              # Go LSP  
+    pyright            # Python LSP
+    terraform-ls       # Terraform LSP
+    yaml-language-server # YAML LSP
   ];
 
+  # Font configuration
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      monospace = [
-        "Hack Nerd Font"
-        "DejaVu Sans Mono"
-      ];
-      sansSerif = [
-        "DejaVu Sans"
-        "Liberation Sans"
-      ];
-      serif = [
-        "DejaVu Serif"
-        "Liberation Serif"
-      ];
+      monospace = [ "Hack Nerd Font" "DejaVu Sans Mono" ];
+      sansSerif = [ "DejaVu Sans" "Liberation Sans" ];
+      serif = [ "DejaVu Serif" "Liberation Serif" ];
     };
   };
 
-  home.activation.rebuildFontCache = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    $VERBOSE_ECHO "Rebuilding font cache..."
-    $DRY_RUN_CMD ${pkgs.fontconfig}/bin/fc-cache -rf
-  '';
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/dbecerra/etc/profile.d/hm-session-vars.sh
-  #
+  # Session variables
   home.sessionVariables = {
     EDITOR = "nvim";
     XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config";
-
   };
-
-  programs.bash = {
-    enable = true;
-    enableCompletion = false;
-
-    initExtra = ''
-      # Load any additional configurations  
-      if [ -f ~/.bashrc.local ]; then
-        source ~/.bashrc.local
-      fi
-    '';
-
-    bashrcExtra = ''
-      # Ble.sh early initialization - MUST be first for interactive shells
-      [[ $- == *i* ]] && [[ -f ${pkgs.blesh}/share/blesh/ble.sh ]] && source ${pkgs.blesh}/share/blesh/ble.sh --attach=none
-
-      ${builtins.readFile ./.bashrc}
-
-      # Ble.sh final attachment - MUST be last
-      [[ ! ''${BLE_VERSION-} ]] || ble-attach
-    '';
-  };
-
-  programs.ssh = {
-    enable = true;
-
-    matchBlocks = {
-      "github.com" = {
-        hostname = "github.com";
-        user = "git";
-        identityFile = "~/.ssh/id_ed25519"; # Path to your existing key
-        identitiesOnly = true; # Only use the specified key
-      };
-    };
-    extraConfig = ''
-      AddKeysToAgent yes
-    '';
-  };
-  services.ssh-agent.enable = true;
-
-  programs.eza = {
-    enable = true;
-    enableBashIntegration = true;
-    git = true;
-    icons = "auto";
-    extraOptions = [
-      "--group-directories-first"
-      "--header"
-    ];
-  };
-
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-
-    extraPackages =
-      with pkgs;
-      [
-        git
-        gcc
-        nodejs
-        python3
-        ruby
-
-        # Core LSP servers based on your extras
-        nil # Nix LSP (lang.nix)
-        lua-language-server # Lua LSP
-        nodePackages.typescript-language-server # TypeScript/JavaScript LSP
-        nodePackages.bash-language-server # Bash LSP
-
-        # Language-specific LSP servers for your extras
-        ansible-language-server # Ansible (lang.ansible)
-        cmake-language-server # CMake (lang.cmake)
-        dockerfile-language-server-nodejs # Docker (lang.docker)
-        gopls # Go (lang.go)
-        nodePackages.vscode-json-languageserver # JSON (lang.json)
-        marksman # Markdown (lang.markdown)
-        pyright # Python (lang.python)
-        terraform-ls # Terraform (lang.terraform)
-        taplo # TOML (lang.toml)
-        vue-language-server # Vue (lang.vue)
-        yaml-language-server # YAML (lang.yaml)
-
-        # Additional LSP servers
-        jsonnet-language-server # For various config files
-        helm-ls # Helm charts
-
-        # Formatters and linters
-        black # Python formatter
-        isort # Python import sorter
-        ruff # Python linter/formatter
-        gofumpt # Go formatter
-        gotools # Go tools (goimports, etc)
-        golangci-lint # Go linter
-        stylua # Lua formatter
-        nodePackages.prettier # General formatter (JS/TS/JSON/MD/YAML)
-        nodePackages.eslint # JavaScript/TypeScript linter
-        rubocop # Ruby linter/formatter
-        shfmt # Shell script formatter
-        shellcheck # Shell script linter
-        hadolint # Dockerfile linter
-        tflint # Terraform linter
-        tfsec # Terraform security scanner
-        yamllint # YAML linter
-        ansible-lint # Ansible linter
-        markdownlint-cli2 # Markdown linter
-
-        # Tools for telescope and other features
-        ripgrep # For telescope
-        fd # For telescope
-        fzf # Fuzzy finder
-        tree-sitter # For treesitter
-
-        # Git tools (lang.git extra)
-        gh # GitHub CLI
-        glab # GitLab CLI
-        delta # Git diff tool
-        lazygit # Git TUI (util.gitui)
-
-        # Additional useful tools
-        curl
-        wget
-        unzip
-        gnutar
-        gzip
-        jq # JSON processor
-        yq # YAML processor
-      ]
-      ++ pkgs.lib.optionals (sqlls != null) [ sqlls ]; # SQL (lang.sql) - if available
-    plugins = with pkgs.vimPlugins; [
-      lazy-nvim
-    ];
-  };
-
-  xdg.configFile."nvim/lua/plugins/mason.lua".text = ''
-    return {
-      -- Disable Mason completely
-      { "williamboman/mason.nvim", enabled = false },
-      { "williamboman/mason-lspconfig.nvim", enabled = false },
-      
-      -- Configure LSP to not use Mason for all your language extras
-      {
-        "neovim/nvim-lspconfig",
-        opts = function(_, opts)
-          opts.servers = opts.servers or {}
-          
-          -- Disable Mason for all servers and configure them
-          local servers = {
-            -- Core
-            "nil_ls",           -- Nix
-            "lua_ls",           -- Lua
-            "tsserver",         -- TypeScript/JavaScript
-            "bashls",           -- Bash
-            
-            -- Your enabled language extras
-            "ansiblels",        -- Ansible
-            "cmake",            -- CMake
-            "dockerls",         -- Docker
-            "gopls",            -- Go
-            "jsonls",           -- JSON
-            "marksman",         -- Markdown
-            "pyright",          -- Python
-            "sqlls",            -- SQL
-            "terraformls",      -- Terraform
-            "taplo",            -- TOML
-            "volar",            -- Vue
-            "yamlls",           -- YAML
-          }
-          
-          for _, server in ipairs(servers) do
-            opts.servers[server] = { mason = false }
-          end
-          
-          return opts
-        end,
-      },
-      
-      -- Configure formatters for all your languages
-      {
-        "stevearc/conform.nvim",
-        opts = function(_, opts)
-          opts.formatters_by_ft = opts.formatters_by_ft or {}
-          
-          -- Configure formatters based on your extras
-          opts.formatters_by_ft.lua = { "stylua" }
-          opts.formatters_by_ft.python = { "black", "isort" }
-          opts.formatters_by_ft.go = { "gofumpt", "goimports" }
-          opts.formatters_by_ft.ruby = { "rubocop" }
-          opts.formatters_by_ft.javascript = { "prettier" }
-          opts.formatters_by_ft.typescript = { "prettier" }
-          opts.formatters_by_ft.vue = { "prettier" }
-          opts.formatters_by_ft.json = { "prettier" }
-          opts.formatters_by_ft.yaml = { "prettier" }
-          opts.formatters_by_ft.markdown = { "prettier" }
-          opts.formatters_by_ft.terraform = { "terraform_fmt" }
-          opts.formatters_by_ft.toml = { "taplo" }
-          opts.formatters_by_ft.sh = { "shfmt" }
-          opts.formatters_by_ft.bash = { "shfmt" }
-          opts.formatters_by_ft.nix = { "nixfmt" }
-          
-          return opts
-        end,
-      },
-      
-      -- Configure linters for all your languages
-      {
-        "mfussenegger/nvim-lint",
-        opts = function(_, opts)
-          opts.linters_by_ft = opts.linters_by_ft or {}
-          
-          -- Configure linters based on your extras
-          opts.linters_by_ft.python = { "ruff" }
-          opts.linters_by_ft.go = { "golangci-lint" }
-          opts.linters_by_ft.ruby = { "rubocop" }
-          opts.linters_by_ft.javascript = { "eslint" }
-          opts.linters_by_ft.typescript = { "eslint" }
-          opts.linters_by_ft.vue = { "eslint" }
-          opts.linters_by_ft.dockerfile = { "hadolint" }
-          opts.linters_by_ft.terraform = { "tflint", "tfsec" }
-          opts.linters_by_ft.yaml = { "yamllint" }
-          opts.linters_by_ft.ansible = { "ansible-lint" }
-          opts.linters_by_ft.sh = { "shellcheck" }
-          opts.linters_by_ft.bash = { "shellcheck" }
-          
-          return opts
-        end,
-      },
-    }
-  '';
-
-  xdg.configFile."nvim/lua/plugins/treesitter.lua".text = ''
-    return {
-      {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-          auto_install = false,  -- Don't auto-install parsers
-          ensure_installed = {}, -- Let Nix handle this
-        },
-      },
-    }
-  '';
-
-  xdg.configFile."nvim/lua/plugins/extras-config.lua".text = ''
-    return {
-      -- Go extra configuration
-      {
-        "ray-x/go.nvim",
-        opts = {
-          goimports = "goimports", -- Use system goimports
-          fillstruct = "fillstruct",
-          dap_debug = false, -- Disable DAP for now
-        },
-      },
-      
-      -- Python extra configuration  
-      {
-        "linux-cultist/venv-selector.nvim",
-        opts = {
-          auto_refresh = true,
-        },
-      },
-      
-      -- Disable any DAP configurations that might conflict
-      {
-        "mfussenegger/nvim-dap",
-        enabled = false, -- Disable for now, can enable later with proper Nix setup
-      },
-      {
-        "rcarriga/nvim-dap-ui", 
-        enabled = false,
-      },
-    }
-  '';
 
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
-  };
-
-  home.file.".config/tmux/tmux.conf".source = "${inputs.oh-my-tmux}/.tmux.conf";
-  home.file.".config/tmux/tmux.conf.local".source = ./tmux.conf.local;
-  #home.shellAliases.tmux = "tmux -f ~/.config/tmux/tmux.conf";
-  home.file.".config/tmux/plugins/catppuccin" = {
-    source = pkgs.fetchFromGitHub {
-      owner = "catppuccin";
-      repo = "tmux";
-      rev = "v2.1.2";
-      hash = "sha256-vBYBvZrMGLpMU059a+Z4SEekWdQD0GrDqBQyqfkEHPg=";
-    };
-    recursive = true;
-  };
-
-  programs.starship = {
-    enable = true;
     enableBashIntegration = true;
-    settings = import ./starship.nix;
   };
 
+  # Tmux with oh-my-tmux
+  home.file.".config/tmux/tmux.conf".source = "${inputs.oh-my-tmux}/.tmux.conf";
+  
   programs.zoxide = {
     enable = true;
     enableBashIntegration = true;
   };
 
-  programs.git = {
-    enable = true;
-    hooks = {
-      pre-commit-custom = pkgs.writeShellScript "pre-commit-custom" ''
-        #!/usr/bin/env bash
-        set -ex
-
-        ${pkgs.pre-commit}/bin/pre-commit run --config "pre-commit-config.yaml" "$@"
-      '';
-    };
-  };
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
