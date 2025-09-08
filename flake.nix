e976ed7a8a87abe1f112e5ee8af0a61beadfe3f1
Davidnbr <davidnbr_98@hotmail.com>
2025-09-08 11:50:35 -0500

    chore: add public keys cachix

--- flake.nix
+++ flake.nix
@@ -14,6 +14,19 @@
     oh-my-tmux = { url = "github:gpakosz/.tmux"; flake = false; };
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://devenv.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
+  };
+
   outputs = { nixpkgs, nixpkgs-stable, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
