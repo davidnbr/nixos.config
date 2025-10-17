7e22c8e00d61aff6ab3a940cfd35d83ea1e8e129
Davidnbr <davidnbr_98@hotmail.com>
2025-10-17 13:55:42 -0500

    chore: fix py in claude-desktop

--- flake.nix
+++ flake.nix
@@ -21,7 +21,8 @@
     claude-desktop = {
       #url = "github:k3d3/claude-desktop-linux-flake";
       #url = "github:davidnbr/claude-desktop-linux-flake/bc43951f8409dd6e461a037b82ca4765b9f1f40d"; ## Workaround graphics with uv fix
-      url = "github:davidnbr/claude-desktop-linux-flake/8d2f7bf9a468d7bb4ec34c3aa717b65e948fc2c5"; # # Fixed graphics
+      #url = "github:davidnbr/claude-desktop-linux-flake/dc16836f2770e3dc6ae6387dec5fd4722f9bcf94"; # # Fixed graphics
+      url = "github:davidnbr/claude-desktop-linux-flake/65bbb75233aef0feceba99aae3b59ad88df666e7"; # # Fixed python3 tkinter
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
