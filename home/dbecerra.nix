3016ed5537e3895af1269b2e9234b7056dfea1ec
Davidnbr <davidnbr_98@hotmail.com>
2026-04-07 12:19:34 -0500

    chore: bump unstable

--- home/dbecerra.nix
+++ home/dbecerra.nix
@@ -36,7 +36,7 @@
     pkgs-unstable.uv
     pkgs-unstable.go
     pkgs-unstable.nodejs_22
-    pkgs-unstable.nodePackages.pnpm
+    pkgs-unstable.pnpm
     pkgs-unstable.volta
     pkgs-unstable.yarn
     pkgs-unstable.ruby_3_4
